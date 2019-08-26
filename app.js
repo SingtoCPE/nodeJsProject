8387cab611ae6a004b296fd28b8bc9bdd3ccda84
thitipat <thitipat.cpe18@hotmail.com>
2019-08-26 15:31:38 +0700

    path param

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const mysql = require('mysql');
-const path = require('path');
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const path = require("path");
 
 let app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
@@ -17,71 +20,90 @@ const port = 3000;
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
-const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: 'singto11442525',
-    database: 'employee'
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "singto11442525",
+  database: "employee"
 });
 
 // connect to database
-app.get('/employee',(req,res)=> {   // Router เวลาเรียกใช้งาน
-    let sql = 'SELECT * FROM employee.tableEmployee;'  // คำสั่ง sql
-    let query = db.query(sql,(err,results) => { // สั่ง Query คำสั่ง sql
-    if(err) throw err  // ดัก error
-    console.log(results[0]) // แสดงผล บน Console
-    res.json(results)   // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
-    })
-    })
+app.get("/employee", (req, res) => {
+  // Router เวลาเรียกใช้งาน
+  let sql = "SELECT * FROM employee.tableEmployee;"; // คำสั่ง sql
+  let query = db.query(sql, (err, results) => {
+    // สั่ง Query คำสั่ง sql
+    if (err) throw err; // ดัก error
+    console.log(results[0]); // แสดงผล บน Console
+    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+  });
+});
 
 // ---------------------------------------------------------
-app.delete('/employee/delete', (req, res) => {
-    let sql = 'DELETE FROM employee.tableEmployee WHERE id=2;'  //adsadad
-    let query = db.query(sql,(err,results) => { // สั่ง Query คำสั่ง sql
-        if(err) throw err  // ดัก error
-        console.log(results[0]) // แสดงผล บน Console
-        res.json(results)   // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
-    })
-})
+app.delete("/employee/delete", (req, res) => {
+  let sql = "DELETE FROM employee.tableEmployee WHERE id=2;"; //adsadad
+  let query = db.query(sql, (err, results) => {
+    // สั่ง Query คำสั่ง sql
+    if (err) throw err; // ดัก error
+    console.log(results[0]); // แสดงผล บน Console
+    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+  });
+});
 // ---------------------------------------------------------
-app.get('/employee/1',(req,res)=> {   // Router เวลาเรียกใช้งาน
-    let sql = 'SELECT * FROM employee.tableEmployee WHERE id=1;'  // คำสั่ง sql
-    let query = db.query(sql,(err,results) => { // สั่ง Query คำสั่ง sql
-    if(err) throw err  // ดัก error
-    console.log(results[0]) // แสดงผล บน Console
-    res.json(results)   // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
-    })
-    })
+app.get("/employee/1", (req, res) => {
+  // Router เวลาเรียกใช้งาน
+  let sql = "SELECT * FROM employee.tableEmployee WHERE id=1;"; // คำสั่ง sql
+  let query = db.query(sql, (err, results) => {
+    // สั่ง Query คำสั่ง sql
+    if (err) throw err; // ดัก error
+    console.log(results[0]); // แสดงผล บน Console
+    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+  });
+});
 // ---------------------------------------------------------
-app.get('/employee/2',(req,res)=> {   // Router เวลาเรียกใช้งาน
-    let sql = 'SELECT * FROM employee.tableEmployee WHERE id=2;'  // คำสั่ง sql
-    let query = db.query(sql,(err,results) => { // สั่ง Query คำสั่ง sql
-    if(err) throw err  // ดัก error
-    console.log(results[0]) // แสดงผล บน Console
-    res.json(results)   // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
-    })
-    })
+app.get("/employee/2", (req, res) => {
+  // Router เวลาเรียกใช้งาน
+  let sql = "SELECT * FROM employee.tableEmployee WHERE id=2;"; // คำสั่ง sql
+  let query = db.query(sql, (err, results) => {
+    // สั่ง Query คำสั่ง sql
+    if (err) throw err; // ดัก error
+    console.log(results[0]); // แสดงผล บน Console
+    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+  });
+});
 // ---------------------------------------------------------
-app.get('/employee/3',(req,res)=> {   // Router เวลาเรียกใช้งาน
-    let sql = 'SELECT * FROM employee.tableEmployee WHERE id=3;'  // คำสั่ง sql
-    let query = db.query(sql,(err,results) => { // สั่ง Query คำสั่ง sql
-    if(err) throw err  // ดัก error
-    console.log(results[0]) // แสดงผล บน Console
-    res.json(results)   // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
-    })
-    })
+app.get("/employee/3", (req, res) => {
+  // Router เวลาเรียกใช้งาน
+  let sql = "SELECT * FROM employee.tableEmployee WHERE id=3;"; // คำสั่ง sql
+  let query = db.query(sql, (err, results) => {
+    // สั่ง Query คำสั่ง sql
+    if (err) throw err; // ดัก error
+    console.log(results[0]); // แสดงผล บน Console
+    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+  });
+});
 // ---------------------------------------------------------
-app.post('/employee/add', (req, res) => {
-    let sql = `INSERT INTO employee.tableEmployee (id,first_name, age) VALUES (3,'test3', '16');`  
-    let query = db.query(sql,(err,results) => { // สั่ง Query คำสั่ง sql
-        if(err) throw err  // ดัก error
-        console.log(results[0]) // แสดงผล บน Console
-        res.json(results)   // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
-    })
-})
-
-
-
+app.get("/employee/:id", (req, res) => {
+  // Router เวลาเรียกใช้งาน
+  let sql = `SELECT * FROM employee.tableEmployee WHERE id=${req.params.id};`; // คำสั่ง sql
+  let query = db.query(sql, (err, results) => {
+    // สั่ง Query คำสั่ง sql
+    if (err) throw err; // ดัก error
+    console.log(results[0]); // แสดงผล บน Console
+    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+  });
+});
+// ---------------------------------------------------------
+// ---------------------------------------------------------
+app.post("/employee/add", (req, res) => {
+  let sql = `INSERT INTO employee.tableEmployee (id,first_name, age) VALUES (3,'test3', '16');`;
+  let query = db.query(sql, (err, results) => {
+    // สั่ง Query คำสั่ง sql
+    if (err) throw err; // ดัก error
+    console.log(results[0]); // แสดงผล บน Console
+    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+  });
+});
 
 //----------------------------------------------------------
 // app.set('port', process.env.port || port); // set express to use this port
@@ -89,7 +111,7 @@ app.post('/employee/add', (req, res) => {
 // app.set('view engine', 'ejs'); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
-app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+app.use(express.static(path.join(__dirname, "public"))); // configure express to use public folder
 app.use(fileUpload()); // configure fileupload
 
 // routes for the app
@@ -104,9 +126,5 @@ app.post('/edit/:id', editPlayer);
 
 // set the app to listen on the port
 app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
+  console.log(`Server running on port: ${port}`);
 });
-
-
-
-
