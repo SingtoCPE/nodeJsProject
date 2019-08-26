6f770ca21bada54d774cb6797c8868141fe0a15a
thitipat <thitipat.cpe18@hotmail.com>
2019-08-26 16:23:52 +0700

    add method insert employee

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json()); // parse form data client
 app.use(express.static(path.join(__dirname, "public"))); // configure express to use public folder
 app.use(fileUpload()); // configure fileupload
 
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -18,13 +17,7 @@ app.use(function(req, res, next) {
   );
   next();
 });
-
-// const {getHomePage} = require('./routes/index');
-// const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
 const port = 3000;
-
-// create connection to database
-// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -32,65 +25,42 @@ const db = mysql.createConnection({
   database: "employee"
 });
 app.get("/employee", (req, res) => {
-  // Router เวลาเรียกใช้งาน
-  let sql = "SELECT * FROM employee.tableEmployee;"; // คำสั่ง sql
+  let sql = "SELECT * FROM employee.tableEmployee;";
   db.query(sql, (err, results) => {
-    // สั่ง Query คำสั่ง sql
-    if (err) throw err; // ดัก error
-    console.log(results[0]); // แสดงผล บน Console
-    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
-  });
-});
-// ---------------------------------------------------------
-app.post("/employee/delete", (req, res) => {
-  let sql = `DELETE FROM employee.tableEmployee WHERE id=${req.body.id};`; //adsadad
-  db.query(sql, (err, results) => {
-    
-    if (err) throw err; 
-    console.log(results[0]); 
-    res.json(results); 
+    if (err) throw err;
+    console.log(results[0]);
+    res.json(results);
   });
 });
 // ---------------------------------------------------------
 app.get("/employee/:id", (req, res) => {
-  // Router เวลาเรียกใช้งาน
-  let sql = `SELECT * FROM employee.tableEmployee WHERE id=${req.params.id};`; // คำสั่ง sql
+  let sql = `SELECT * FROM employee.tableEmployee WHERE id=${req.params.id};`;
   db.query(sql, (err, results) => {
-    // สั่ง Query คำสั่ง sql
-    if (err) throw err; // ดัก error
-    console.log(results[0]); // แสดงผล บน Console
-    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+    if (err) throw err;
+    console.log(results[0]);
+    res.json(results);
   });
 });
 // ---------------------------------------------------------
+app.post("/employee/delete", (req, res) => {
+  let sql = `DELETE FROM employee.tableEmployee WHERE id=${req.body.id};`;
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    console.log(results[0]);
+    res.json(results);
+  });
+});
 // ---------------------------------------------------------
 app.post("/employee/add", (req, res) => {
-  let sql = `INSERT INTO employee.tableEmployee (id,first_name, age) VALUES (3,'test3', '16');`;
+  console.log(req);
+  let sql = `INSERT INTO employee.tableEmployee (first_name, age) VALUES ('${req.body.first_name}', ${req.body.age});`;
   db.query(sql, (err, results) => {
-    // สั่ง Query คำสั่ง sql
-    if (err) throw err; // ดัก error
-    console.log(results[0]); // แสดงผล บน Console
-    res.json(results); // สร้างผลลัพธ์เป็น JSON ส่งออกไปบน Browser
+    if (err) throw err;
+    console.log(results[0]);
+    res.json(results);
   });
 });
 
-//----------------------------------------------------------
-// app.set('port', process.env.port || port); // set express to use this port
-// app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
-// app.set('view engine', 'ejs'); // configure template engine
-
-
-// routes for the app
-/*
-app.get('/', getHomePage);
-app.get('/add', addPlayerPage);
-app.get('/edit/:id', editPlayerPage);
-app.get('/delete/:id', deletePlayer);
-app.post('/add', addPlayer);
-app.post('/edit/:id', editPlayer);
-*/
-
-// set the app to listen on the port
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
